c31fdbde238c31cf945db77d734e3a13378689a1
wanjohialvins <wanjohialvinsgmail.com>
2026-01-03 00:24:03 +0300

    Implemented consecutiveSubstrings logic, pseudocode, and explanation

--- consecutive_substrings.js
+++ consecutive_substrings.js
@@ -1,5 +1,13 @@
 function consecutiveSubstrings(string) {
-  // type your code here
+  const result = [];
+
+  for (let i = 0; i < string.length; i++) {
+    for (let j = i + 1; j <= string.length; j++) {
+      result.push(string.slice(i, j));
+    }
+  }
+
+  return result;
 }
 
 if (require.main === module) {
